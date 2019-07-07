642af000b681d02009d7043a0ed3611c1f7697f8
Dylan Michael Labat <dml252@live.com>
2019-07-07 16:44:30 -0500

    Add helper methods to application controller, move logout to app footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :require_login, only: [:show, :edit]
+  helper_method :logged_in?, :current_user
+
+  private
+
+  def require_login
+    if !logged_in?
+      redirect_to root_path
+    end
+  end
+
+  def logged_in?
+    session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
