51fe102d2a87027975b725c0ef0de2132913301f
Dylan Michael Labat <dml252@live.com>
2019-07-13 09:32:02 -0500

    Add new, create, and show methods to listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,26 @@
 class ListingsController < ApplicationController
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+     if @listing.save
+       redirect_to @listing
+     else
+       redirect_to new_listing_path
+     end
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:condition, :price, :user_id, :album_id)
+  end
+
 end
