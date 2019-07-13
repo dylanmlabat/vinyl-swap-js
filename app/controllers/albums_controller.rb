49e8f71d47482e0a93beaf0b69460e9a441c3279
Dylan Michael Labat <dml252@live.com>
2019-07-13 08:55:33 -0500

    Add new, create, and show methods to albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,26 @@
 class AlbumsController < ApplicationController
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+     if @album.save
+       redirect_to @album
+     else
+       redirect_to new_album_path
+     end
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  private
+
+  def album_params
+    params.require(:user).permit(:artist, :title, :release_year, :genre)
+  end
+
 end
