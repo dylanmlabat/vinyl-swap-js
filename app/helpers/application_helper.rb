8520a0fb7e07f7e1ab856fd4c2b1acd0d27d6fa4
Dylan Michael Labat <dml252@live.com>
2019-07-19 18:23:56 -0500

    Add helper method+css styles for inline errors, add to signup form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def inline_errors(model, attribute)
+    result = ""
+    if model.errors[attribute].any?
+         model.errors[attribute].each do |message|
+            result += "<li>#{message}</li>"
+        end
+    end
+    return "<ul>#{result}</ul>".html_safe
+  end
+
 end
