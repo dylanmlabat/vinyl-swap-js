6dd53d448db9e766fc8738221725efc3982ae30c
Dylan Michael Labat <dml252@live.com>
2019-09-11 11:54:30 -0500

    Add album constructor function to album.js

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -1,3 +1,14 @@
 $(function(){
   console.log('albums.js is loaded...')
 })
+
+class Album {
+  constructor(album){
+    this.id = album.id
+    this.artist = album.artist
+    this.title = album.title
+    this.release_year = album.release_year
+    this.genre = album.genre
+    this.listings = album.listings
+  }
+}
