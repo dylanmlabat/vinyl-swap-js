cf2c0752ec1628c59b1f6302e33e0950c2fc90be
Dylan Michael Labat <dml252@live.com>
2019-09-11 15:22:06 -0500

    Add listingHTML function to listing.js, fix errors in constructor

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -17,11 +17,13 @@ function getListings(){
     dataType: 'json'
   }).done(function(data){
     console.log('the data is: ', data)
+    let testlisting = new Listing(data[0])
+    let myListingHTML = testlisting.listingHTML()
     debugger
   })
 }
 
-class Listings {
+class Listing {
   constructor(listing){
     this.id = listing.id
     this.condition = listing.condition
@@ -29,7 +31,15 @@ class Listings {
     this.notes = listing.notes
     this.user_id = listing.user_id
     this.album_id = listing.album_id
-    this.users = listing.users
-    this.albums = listing.albums
+    this.user = listing.user
+    this.album = listing.album
   }
+}
+
+Listing.prototype.listingHTML = function() {
+  return (`
+    ${this.album.artist} - ${this.album.title}<br>
+    <small>Sold by ${this.user.username}</small><br>
+    <small>${this.condition} - $${this.price}</small>
+  `)
 }
